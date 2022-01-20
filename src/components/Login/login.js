345c0705e4a9bb85663960afdad5e76ee7cfd339
iamyogeshmalkani <conan.yogesh@gmail.com>
2022-01-20 10:00:51 +0530

    wave added

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -8,7 +8,10 @@ import LockIcon from '@mui/icons-material/Lock';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 
 import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
 import Link from '@mui/material/Link';
+import background from "../../icons/wave.jpg"
+import { color } from '@mui/system';
 
 // import {ReactComponent as ReactLogo} from "../../icons/wave.svg";
 
@@ -29,8 +32,17 @@ export default function Login() {
 <div className="Login">
   
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.8" d="M0,288L34.3,261.3C68.6,235,137,181,206,170.7C274.3,160,343,192,411,202.7C480,213,549,203,617,165.3C685.7,128,754,64,823,58.7C891.4,53,960,107,1029,144C1097.1,181,1166,203,1234,213.3C1302.9,224,1371,224,1406,224L1440,224L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path></svg>
-<h1>Welcome hrami</h1>
+
+<div className="loginheader" style={{backgroundImage:`url(${background})`,backgroundSize: "cover"}} >
+
+<h1>Welcome To<br></br> Five Chan</h1>
+<PersonIcon
+className="personicon"
+
+ />
+
+
+</div>
   
    {/* <ReactLogo/> */}
       
@@ -43,6 +55,7 @@ export default function Login() {
         }}  />
        
        <TextField id="outlined-basic" label="Password" type={type} value={password} variant="outlined" onChange={(e)=>{handlechange(e)}}  InputProps={{
+        
           startAdornment: (
             <InputAdornment position="start">
               <LockIcon />
@@ -61,8 +74,14 @@ export default function Login() {
             </InputAdornment>
           ),
         }} />
-       <Button variant="contained">Login</Button>
-       <Link href="/signup" underline="none">
+       <Button variant="contained" className="Loginbutton"   style={{
+       backgroundColor: "skyblue",
+       color: "white"
+    }} >Login</Button>
+       <Link href="/signup" underline="none" style={{
+       
+       color: "skyblue"
+    }}>
         {"New User Register Here"}
       </Link>
   </div>
--- src/components/Login/signup.js
+++ src/components/Login/signup.js
@@ -13,44 +13,55 @@ import { Password, Person, Visibility } from "@mui/icons-material";
 import LockIcon from '@mui/icons-material/Lock';
 import IconButton from '@mui/material/IconButton';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import background from "../../icons/wave.jpg"
+import PersonIcon from '@mui/icons-material/Person';
 
 const Signup = () => {
-  const [type,settype] = useState("password");
-  const [typeconfirm,settypeconfirm] = useState("password");
+  const [type, settype] = useState("password");
+  const [typeconfirm, settypeconfirm] = useState("password");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
   const [confirmpass, setconfirmPass] = useState("");
 
-  function showpassword(str){
-    if(str=="pass"){
-      type=="password" ? settype("text") : settype("password");
+  function showpassword(str) {
+    if (str == "pass") {
+      type == "password" ? settype("text") : settype("password");
 
     }
-    else{
-      typeconfirm=="password" ? settypeconfirm("text") : settypeconfirm("password");
+    else {
+      typeconfirm == "password" ? settypeconfirm("text") : settypeconfirm("password");
 
     }
-    
+
 
   }
-  function handlechange(evt,str){
-    if(str==="pass"){
+  function handlechange(evt, str) {
+    if (str === "pass") {
       setpassword(evt.target.value);
     }
-    else{
+    else {
       setconfirmPass(evt.target.value);
     }
-   
-    
-  
+
+
+
   }
- 
+
 
   return (
     <>
       <div className="Login">
-        <h1 className="heading">SIGN-UP</h1>
+        <div className="loginheader" style={{ backgroundImage: `url(${background})`, backgroundSize: "cover" }} >
+
+          <h1>Welcome To<br></br> Five Chan</h1>
+          <PersonIcon
+            className="personicon"
+
+          />
+
+
+        </div>
         <Box
           component="form"
           sx={{
@@ -59,93 +70,99 @@ const Signup = () => {
           noValidate
           autoComplete="off"
         />
-       
-          <TextField
-            required
-            id="outlined-required"
-            label="Username"
-            defaultValue=""
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Person />
-                </InputAdornment>
-              ),
-            }}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        
 
-        
-          <TextField
-            required
-            id="outlined-required"
-            label="Email"
-            defaultValue=""
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Email />
-                </InputAdornment>
-              ),
-            }}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        
+        <TextField
+          required
+          id="outlined-required"
+          label="Username"
+          defaultValue=""
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Person />
+              </InputAdornment>
+            ),
+          }}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-        
-        <TextField id="outlined-basic" label="Password" type={type} value={password} variant="outlined" onChange={(e)=>{handlechange(e,"pass")}}  InputProps={{
+
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          defaultValue=""
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            ),
+          }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+
+
+        <TextField id="outlined-basic" label="Password" type={type} value={password} variant="outlined" onChange={(e) => { handlechange(e, "pass") }} InputProps={{
           startAdornment: (
             <InputAdornment position="start">
               <LockIcon />
             </InputAdornment>
           ),
-          
-          endAdornment: (
-            
-            <InputAdornment position="end">
-              {password!="" ? <IconButton onClick={()=>{showpassword("pass")}}>
-              <VisibilityIcon />
 
-              </IconButton>:null}
-              
-             
+          endAdornment: (
+
+            <InputAdornment position="end">
+              {password != "" ? <IconButton onClick={() => { showpassword("pass") }}>
+                <VisibilityIcon />
+
+              </IconButton> : null}
+
+
             </InputAdornment>
           ),
         }} />
-       
 
-       
-       <TextField id="outlined-basic" label="Confirm Password" type={typeconfirm} value={confirmpass} variant="outlined" onChange={(e)=>{handlechange(e,"confirmpass")}}  InputProps={{
+
+
+        <TextField id="outlined-basic" label="Confirm Password" type={typeconfirm} value={confirmpass} variant="outlined" onChange={(e) => { handlechange(e, "confirmpass") }} InputProps={{
           startAdornment: (
             <InputAdornment position="start">
               <LockIcon />
             </InputAdornment>
           ),
-          
-          endAdornment: (
-            
-            <InputAdornment position="end">
-              {password!="" ? <IconButton onClick={()=>{showpassword("confirmpass")}}>
-              <VisibilityIcon />
 
-              </IconButton>:null}
-              
-             
+          endAdornment: (
+
+            <InputAdornment position="end">
+              {password != "" ? <IconButton onClick={() => { showpassword("confirmpass") }}>
+                <VisibilityIcon />
+
+              </IconButton> : null}
+
+
             </InputAdornment>
           ),
         }} />
-       
-        
-          <Button variant="contained">Signup</Button>
-          <Link href="/login" underline="none">
-              {"Already have an account"}
-          </Link>
-          
-         
-        
+
+
+        <Button variant="contained" style={{
+          backgroundColor: "skyblue",
+          color: "white"
+        }} >Signup</Button>
+        <Link href="/login" underline="none" style={{
+
+          color: "skyblue"
+        }}>
+          {"Already have an account"}
+        </Link>
+
+
+
       </div>
     </>
   );
